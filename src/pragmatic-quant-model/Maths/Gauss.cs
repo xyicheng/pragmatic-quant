71e64eb2d8e2e30e8de59713c3ffbff6f167f34c
quintron <olivier.drevillon@gmail.com>
2014-11-28 16:56:05 +0100

    test git

---
+++ src/pragmatic-quant-model/Maths/Gauss.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pragmatic_quant_model.Maths
+{
+    class Gauss
+    {
+    }
+}
