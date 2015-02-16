22d28046ede3ec7dac5992c6c77f7733362c4e4b
quintron <olivier.drevillon@gmail.com>
2015-02-16 21:04:03 +0100

    sobol unit test

--- src/test/Maths/BrownianBridge_Test.cs
+++ src/test/Maths/BrownianBridge_Test.cs
@@ -53,7 +53,7 @@ namespace test.Maths
         }
 
         #region private method
-        private void MultiDimPathVariance(double[] dates, int brownianDim, int nbPaths, double precision)
+        private void MultiDimPathCovariance(double[] dates, int brownianDim, int nbPaths, double precision)
         {
             var brownian = BrownianBridge.Create(dates);
 
@@ -61,39 +61,39 @@ namespace test.Maths
             chrono.Start();
 
             var sobol = new SobolRsg(dates.Length * brownianDim, 0, SobolRsg.DirectionIntegers.JoeKuoD5);
-            var variances = new double[dates.Length][,];
-            for (int j = 0; j < dates.Length; ++j) variances[j] = new double[brownianDim, brownianDim];
+            var covariances = new double[dates.Length][,];
+            for (int j = 0; j < dates.Length; ++j) covariances[j] = new double[brownianDim, brownianDim];
 
             for (int i = 0; i < nbPaths; ++i)
             {
                 var uniforms = sobol.NextSequence();
                 var gaussians = uniforms.Select(NormalDistribution.CumulativeInverse).ToArray();
                 var path = brownian.NextPath(gaussians, brownianDim);
-                for (int j = 0; j < variances.Length; ++j)
+                for (int j = 0; j < covariances.Length; ++j)
                 {
-                    VectorUtils.AddCov(ref variances[j], path[j], path[j]);
+                    VectorUtils.AddCov(ref covariances[j], path[j], path[j]);
                 }
             }
-            for (int j = 0; j < variances.Length; ++j)
+            for (int j = 0; j < covariances.Length; ++j)
             {
-                MatrixUtils.MultTo(ref variances[j], 1.0 / nbPaths);
+                MatrixUtils.MultTo(ref covariances[j], 1.0 / nbPaths);
             }
 
             chrono.Stop();
             Console.WriteLine("Elapsed " + chrono.Elapsed);
 
-            for (int j = 0; j < variances.Length; ++j)
+            for (int j = 0; j < covariances.Length; ++j)
             {
                 var covRef = MatrixUtils.Diagonal(brownianDim, dates[j]);
-                UnitTestUtils.EqualDoubleMatrix(variances[j], covRef, precision);
+                UnitTestUtils.EqualDoubleMatrix(covariances[j], covRef, precision);
             }
         }
         #endregion
         [TestMethod]
-        public void MultiDimPathVariance()
+        public void MultiDimPathCovariance()
         {
             var dates = new[] { 0.0, 0.083333333333, 0.25, 0.5, 0.901, 2.0, 3.10184, 4.0, 5.0 };
-            MultiDimPathVariance(dates, 3, 1000000, 2.5e-4);
+            MultiDimPathCovariance(dates, 3, 1000000, 2.5e-4);
         }
     }
 }
--- src/test/Maths/Sobol_Test.cs
+++ src/test/Maths/Sobol_Test.cs
@@ -11,6 +11,45 @@ namespace test.Maths
     public class Sobol_Test
     {
         [TestMethod]
+        public void InitialisationTest()
+        {
+            var chrono = new Stopwatch();
+            chrono.Start();
+
+            var jaeckel = new SobolRsg(32, 0, SobolRsg.DirectionIntegers.Jaeckel);
+            jaeckel.NextSequence();
+
+            var sobolLevitan = new SobolRsg(40, 0, SobolRsg.DirectionIntegers.SobolLevitan);
+            sobolLevitan.NextSequence();
+
+            var sobolLevitanLemieux = new SobolRsg(360, 0, SobolRsg.DirectionIntegers.SobolLevitanLemieux);
+            sobolLevitanLemieux.NextSequence();
+
+            var joeKuoD5 = new SobolRsg(1999, 0, SobolRsg.DirectionIntegers.JoeKuoD5);
+            joeKuoD5.NextSequence();
+
+            var joeKuoD6 = new SobolRsg(1799, 0, SobolRsg.DirectionIntegers.JoeKuoD6);
+            joeKuoD6.NextSequence();
+
+            var joeKuoD7 = new SobolRsg(1899, 0, SobolRsg.DirectionIntegers.JoeKuoD7);
+            joeKuoD7.NextSequence();
+
+            var kuo = new SobolRsg(4925, 0, SobolRsg.DirectionIntegers.Kuo);
+            kuo.NextSequence();
+
+            var kuo2 = new SobolRsg(3946, 0, SobolRsg.DirectionIntegers.Kuo2);
+            kuo2.NextSequence();
+
+            var kuo3 = new SobolRsg(4586, 0, SobolRsg.DirectionIntegers.Kuo3);
+            kuo3.NextSequence();
+
+            chrono.Stop();
+            Console.WriteLine("Elapsed " + chrono.Elapsed);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
         public void Test1()
         {
             const int dim = 10;
@@ -43,9 +82,11 @@ namespace test.Maths
             var chrono = new Stopwatch();
             chrono.Start();    
 
-            double[] atmCalls = new double[dim];
-            double[] strike1Call = new double[dim];
-            double[] strike2Call = new double[dim];
+            var atmCalls = new double[dim];
+            var strikeCall1 = new double[dim];
+            var strikeCall2 = new double[dim];
+            var strikePut1 = new double[dim];
+            var strikePut2 = new double[dim];
             const int nbPaths = 1000000;
             for (int index = 0; index < nbPaths; index++)
             {
@@ -54,27 +95,35 @@ namespace test.Maths
                 {
                     var gaussian = NormalDistribution.CumulativeInverse(sample[i]);
                     atmCalls[i] += Math.Max(0.0, gaussian);
-                    strike1Call[i] += Math.Max(0.0, gaussian - 1.0);
-                    strike2Call[i] += Math.Max(0.0, gaussian - 2.0);
+                    strikeCall1[i] += Math.Max(0.0, gaussian - 1.0);
+                    strikeCall2[i] += Math.Max(0.0, gaussian - 2.0);
+                    strikePut1[i] += Math.Max(0.0, -1.0 - gaussian);
+                    strikePut2[i] += Math.Max(0.0, -2.0 - gaussian);
                 }
             }
             for (int i = 0; i < dim; i++)
             {
                 atmCalls[i] /= nbPaths;
-                strike1Call[i] /= nbPaths;
-                strike2Call[i] /= nbPaths;
+                strikeCall1[i] /= nbPaths;
+                strikeCall2[i] /= nbPaths;
+                strikePut1[i] /= nbPaths;
+                strikePut2[i] /= nbPaths;
             }
 
             chrono.Stop();
             Console.WriteLine("Elapsed " + chrono.Elapsed);
 
-            var refAtmCalls = atmCalls.Select(c => BachelierOption.Price(0.0, 0.0, 1.0, 1.0, 1.0)).ToArray();
-            var refStrike1Calls = atmCalls.Select(c => BachelierOption.Price(0.0, 1.0, 1.0, 1.0, 1.0)).ToArray();
-            var refStrike2Calls = atmCalls.Select(c => BachelierOption.Price(0.0, 2.0, 1.0, 1.0, 1.0)).ToArray();
+            var refAtmCalls = atmCalls.Select(c => BachelierOption.Price(0.0, 0.0, 1.0, 1.0, 1)).ToArray();
+            var refStrikeCall1 = atmCalls.Select(c => BachelierOption.Price(0.0, 1.0, 1.0, 1.0, 1)).ToArray();
+            var refStrikeCall2 = atmCalls.Select(c => BachelierOption.Price(0.0, 2.0, 1.0, 1.0, 1)).ToArray();
+            var refStrikePut1 = atmCalls.Select(c => BachelierOption.Price(0.0, -1.0, 1.0, 1.0, -1)).ToArray();
+            var refStrikePut2 = atmCalls.Select(c => BachelierOption.Price(0.0, -2.0, 1.0, 1.0, -1)).ToArray();
             
             UnitTestUtils.EqualDoubleArray(atmCalls, refAtmCalls, 2.0e-5, true);
-            UnitTestUtils.EqualDoubleArray(strike1Call, refStrike1Calls, 8.0e-5, true);
-            UnitTestUtils.EqualDoubleArray(strike2Call, refStrike2Calls, 6.0e-4, true);
+            UnitTestUtils.EqualDoubleArray(strikeCall1, refStrikeCall1, 8.0e-5, true);
+            UnitTestUtils.EqualDoubleArray(strikeCall2, refStrikeCall2, 6.0e-4, true);
+            UnitTestUtils.EqualDoubleArray(strikePut1, refStrikePut1, 8.0e-5, true);
+            UnitTestUtils.EqualDoubleArray(strikePut2, refStrikePut2, 6.0e-4, true);
         }
     }
 }
