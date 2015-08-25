0eb4ce00d4a9ee489a903f23f16269026820f8c2
quintron <olivier.drevillon@gmail.com>
2015-08-25 22:54:35 +0200

    optimization of DslCoupon evaluation

--- src/pragmatic-quant-model/Product/CouponDsl/DslCoupon.cs
+++ src/pragmatic-quant-model/Product/CouponDsl/DslCoupon.cs
@@ -1,21 +1,24 @@
-using System.Reflection;
+using System;
 
 namespace pragmatic_quant_model.Product.CouponDsl
 {
     public class DslCoupon : Coupon
     {
         #region private fields
-        private readonly MethodInfo payoff;
+        private readonly Func<object, double[], double> payoff;
+        private readonly object payoffObj;
         #endregion
-        public DslCoupon(PaymentInfo paymentInfo, IFixing[] fixings, MethodInfo payoff)
+        public DslCoupon(PaymentInfo paymentInfo, IFixing[] fixings,
+                         Func<object, double[], double> payoff, object payoffObj)
             : base(paymentInfo, fixings)
         {
             this.payoff = payoff;
+            this.payoffObj = payoffObj;
         }
 
         public override double Payoff(double[] fixings)
         {
-            return (double) payoff.Invoke(null, new object[] {fixings});
+            return payoff(payoffObj, fixings);
         }
     }
 }
--- src/pragmatic-quant-model/Product/CouponDsl/DslCouponCompiler.cs
+++ src/pragmatic-quant-model/Product/CouponDsl/DslCouponCompiler.cs
@@ -2,6 +2,7 @@ using System;
 using System.CodeDom.Compiler;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using Microsoft.CSharp;
 using pragmatic_quant_model.Basic;
@@ -11,36 +12,31 @@ namespace pragmatic_quant_model.Product.CouponDsl
     public static class DslCouponCompiler
     {
         #region private fields
-        private const string PayoffClassName = "GeneratedCouponDslPayoff";
-        private static int _methodIdentifier;
+        private static int _classIdentifier;
         #endregion
         #region private methods
-        private static string GetMethodId()
-        {
-            return string.Format("Payoff{0}", _methodIdentifier++);
-        }
-        private static string GenerateCode(DslPayoffExpression[] expressions, string[] methodIds)
+        private static string GenerateCode(DslPayoffExpression[] expressions, string className, string[] methodIds)
         {
             StringBuilder code = new StringBuilder();
             code.AppendLine("using System;");
-            code.AppendLine(string.Format("public static partial class {0}", PayoffClassName));
+            code.AppendLine(String.Format("public class {0}", className));
             code.AppendLine("{");
 
             for (int i = 0; i < expressions.Length; i++)
             {
                 //Payoff Methods
-                code.AppendLine(string.Format("public static double {0}(double[] {1})",
+                code.AppendLine(String.Format("     public double {0}(double[] {1})",
                     methodIds[i], expressions[i].FixingArrayId));
-                code.AppendLine("{");
-                code.AppendLine("return " + expressions[i].Expression + ";");
-                code.AppendLine("}");
+                code.AppendLine("   {");
+                code.AppendLine("       return " + expressions[i].Expression + ";");
+                code.AppendLine("   }");
             }
 
             code.AppendLine("}");
             string generatedCode = code.ToString();
             return generatedCode;
         }
-        private static Type Compile(string generatedCode)
+        private static Type Compile(string generatedCode, string payoffClassName)
         {
             // Prepare the compiler.
             CompilerParameters parameters = new CompilerParameters
@@ -64,22 +60,58 @@ namespace pragmatic_quant_model.Product.CouponDsl
 
             // Locate the method we've just defined (cf. CodeGenerator).
             return results.CompiledAssembly.GetModules()[0]
-                                           .GetType(PayoffClassName);
+                                           .GetType(payoffClassName);
+        }
+        /// <summary>
+        /// Code from Ben Watson's book : "Writing High-Performance .Net Code"
+        /// </summary>
+        public static T GenerateMethodCall<T>(MethodInfo methodInfo,
+                                              Type extensionType,
+                                              Type returnType,
+                                              Type[] parametersTypes) where T : class
+        {
+            
+
+            var dynamicMethod = new DynamicMethod("Invoke_" + methodInfo.Name, returnType, parametersTypes, true);
+            var ilGenerator = dynamicMethod.GetILGenerator();
+
+            ilGenerator.DeclareLocal(extensionType);
+            // object's this parameter
+            ilGenerator.Emit(OpCodes.Ldarg_0);
+            // cast it to the correct type
+            ilGenerator.Emit(OpCodes.Castclass, extensionType);
+            // actual method argument
+            ilGenerator.Emit(OpCodes.Stloc_0);
+            ilGenerator.Emit(OpCodes.Ldloc_0);
+            ilGenerator.Emit(OpCodes.Ldarg_1);
+            ilGenerator.EmitCall(OpCodes.Callvirt, methodInfo, null);
+            ilGenerator.Emit(OpCodes.Ret);
+
+            object del = dynamicMethod.CreateDelegate(typeof(T));
+            return (T)del;
         }
         #endregion
 
         public static DslCoupon[] Compile(params DslCouponData[] dslCouponDatas)
         {
-            var methodIds = EnumerableUtils.For(0, dslCouponDatas.Length, i => GetMethodId());
-            string generatedCode = GenerateCode(dslCouponDatas.Map(d => d.Expression), methodIds);
-            Type methodClassContainer = Compile(generatedCode);
-            MethodInfo[] payoffMethods = methodIds.Map(methodId => methodClassContainer.GetMethod(methodId));
+            string[] methodIds = EnumerableUtils.For(0, dslCouponDatas.Length, i => String.Format("Payoff{0}", i));
+            string payoffclassName = String.Format("GeneratedDslPayoff{0}", _classIdentifier++);
+            
+            string generatedCode = GenerateCode(dslCouponDatas.Map(d => d.Expression), payoffclassName, methodIds);
+            Type payoffClassType = Compile(generatedCode, payoffclassName);
+
+            object payoffObj = Activator.CreateInstance(payoffClassType);
+            MethodInfo[] payoffMethods = methodIds.Map(methodId => payoffClassType.GetMethod(methodId));
 
             return EnumerableUtils.For(0, dslCouponDatas.Length, i =>
             {
                 var fixings = dslCouponDatas[i].Expression.Fixings.ToArray();
                 var paymentInfo = dslCouponDatas[i].PaymentInfo;
-                return new DslCoupon(paymentInfo, fixings, payoffMethods[i]);
+
+                var payoff = GenerateMethodCall<Func<object, double[], double>>
+                    (payoffMethods[i], payoffClassType, typeof (double), new[] {typeof (object), typeof (double[])});
+                
+                return new DslCoupon(paymentInfo, fixings, payoff, payoffObj);
             });
         }
     }
